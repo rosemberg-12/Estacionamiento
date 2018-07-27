5d50210be0ee2e78914592b2c036db25595bf241
rosemberg.porras <rosemberg.porras@ceiba.com.co>
2018-07-27 11:09:51 -0500

    Demo application updated

---
+++ src/main/java/com/ceiba/parking/parking/ParkingApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.parking.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApplication.class, args);
+	}
+}
