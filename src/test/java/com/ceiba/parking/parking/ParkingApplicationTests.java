29a7eb8cace0dae0dc64974211d88bddc8dee18b
rosemberg.porras <rosemberg.porras@ceiba.com.co>
2018-07-27 11:24:23 -0500

    First example test updated

--- src/test/java/com/ceiba/parking/parking/ParkingApplicationTests.java
+++ src/test/java/com/ceiba/parking/parking/ParkingApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ceiba.parking.parking;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ParkingApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Test 
+	public void initTest(){
+		Assert.assertTrue("This is a test, yei", true);
 	}
 
 }
