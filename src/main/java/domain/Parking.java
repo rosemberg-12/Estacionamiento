393a9036b708fe16a1e4e2c3bd43211a2ff9f8a4
rosemberg.porras <rosemberg.porras@ceiba.com.co>
2018-07-27 14:10:24 -0500

    Extra configuration of sonar added

---
+++ src/main/java/domain/Parking.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Parking {
+	
+	/**
+	 * Define if the parking is closed
+	 * @return
+	 */
+	public boolean isClosed(){
+		return true;
+	}
+
+}
--- src/test/java/com/ceiba/parking/parking/ParkingApplicationTests.java
+++ src/test/java/com/ceiba/parking/parking/ParkingApplicationTests.java
@@ -6,14 +6,24 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import domain.Parking;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ParkingApplicationTests {
 
 	@Test 
 	public void initTest(){
-		System.out.println("Executing test....");
 		Assert.assertTrue("This is a test, yei", true);
+	}
+	
+	@Test
+	/**
+	 * Test about static parameter "closed"
+	 */
+	public void parkingClosedTest(){
+		Parking p=new Parking();
+		Assert.assertTrue(p.isClosed());
 	}
 
 }
