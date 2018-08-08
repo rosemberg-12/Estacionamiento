32ac6111428fe21c597261c8e530f694d04a73f9
rosemberg.porras <rosemberg.porras@ceiba.com.co>
2018-08-08 08:16:14 -0500

    removed unused code

--- src/main/java/com/ceiba/parking/transport/DefaultRestResponse.java
+++ src/main/java/com/ceiba/parking/transport/DefaultRestResponse.java
@@ -10,17 +10,7 @@ public class DefaultRestResponse {
 	private boolean response;
 	private String nameException;
 	private String messageException;
-	
-	public DefaultRestResponse(){
-		
-	}
-	
-	protected DefaultRestResponse(boolean response, String nameException, String messageException) {
-		super();
-		this.response = response;
-		this.nameException = nameException;
-		this.messageException = messageException;
-	}
+
 	
 	public boolean isResponse() {
 		return response;
