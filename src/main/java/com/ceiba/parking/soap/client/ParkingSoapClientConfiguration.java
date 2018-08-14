c64b93e6f609c77e2f760cdfe3b3ca5b0eb55b9e
rosemberg.porras <rosemberg.porras@ceiba.com.co>
2018-08-14 15:01:42 -0500

    Removed comment

--- src/main/java/com/ceiba/parking/soap/client/ParkingSoapClientConfiguration.java
+++ src/main/java/com/ceiba/parking/soap/client/ParkingSoapClientConfiguration.java
@@ -10,8 +10,6 @@ public class ParkingSoapClientConfiguration {
 	@Bean
 	public Jaxb2Marshaller marshaller() {
 		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		// this package must match the package in the <generatePackage> specified in
-		// pom.xml
 		marshaller.setContextPath("com.ceiba.parking.wsdl");
 		return marshaller;
 	}
